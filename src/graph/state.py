680e595189895ec489b5a8665e31e169a40a7485
Tarun Nagpal <tarun.nagpal@simformsolutions.com>
2026-06-01 11:02:03 +0530

    fix state code which return in graph

---
+++ src/graph/state.py
@@ -0,0 +1,12 @@
+from typing import TypedDict
+# from src.config.constants.intents import Intent
+
+class BookingState(TypedDict):
+    messages : list[str]
+    user_id : str
+    intent : str
+    city : str | None
+    search_results : list | None
+    booking_draft : dict | None
+    confirmed : bool | None
+    error_message : str | None
